6f31a701d18740ad8444942c203ec4be9f67b6b9
Levi Shaffer <gobslayer@gmail.com>
2020-09-13 21:10:18 -0500

    Fixed string tokenization, debugging Param::printParams()

--- parse.cpp
+++ parse.cpp
@@ -1,23 +1,40 @@
 #include "parse.hpp"
-#include <string.h>
-#include <iostream>
 
 Parser::Parser () { }
 
-void Parser::parseString(char* str) {
-    // std::vector<char*> tokens;
-    const char delim[2] = " ";
-    char* res = strtok(str, delim);
-    // tokens.push_back(res);
-    // int len = 1;
+void Parser::parseString(std::string str) {
+    std::vector<char*> tokens;
+    std::replace(str.begin(), str.end(), '\t', ' ');
+    std::replace(str.begin(), str.end(), '\n', ' ');
+    std::replace(str.begin(), str.end(), '\r', ' ');
+    char delim[] = " ";
+    char* w_str = new char[str.length() + 1];
+    strcpy(w_str, str.c_str()); // ew
+    char* res = strtok(w_str, delim);
     while(res != NULL) {
         std::cout << res << std::endl;
-        res = strtok(str, " ");
+        tokens.push_back(res);
+        res = strtok(NULL, " ");
     }
+    delete w_str; // only you can prevent memory leaks
+    tokensToParam(tokens);
+}
+
+void Parser::tokensToParam(const std::vector<char*>& vec) {
+    parms.argumentCount = vec.size();
+    for(size_t i = 0; i < vec.size(); i++) {
+        if(vec.at(i)[0] == '<') {
+            parms.inputRedirect = vec.at(i);
+        } else if (vec.at(i)[0] == '>') {
+            parms.outputRedirect = vec.at(i);
+        }
+        parms.argumentVector[i] = vec.at(i);
+    }
+    parms.printParams();
 }
 
 int main() {
     Parser parse;
-    char* testStr = "prog arg1 arg2 arg3 arg4";
+    std::string testStr = "prog arg1\targ2\narg3    >arg4";
     parse.parseString(testStr);
 }
